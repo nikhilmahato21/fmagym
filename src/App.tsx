ce385e2cee68ac81a3825c6166a952251c95f68b
nikhilmahato21 <nikhilmahato3337@gmail.com>
2025-11-26 21:40:48 +0530

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { Menu, X, Check, MapPin, Phone, Mail, ArrowRight, Star, Dumbbell, ChevronRight } from 'lucide-react';
 
 const FmaGymLanding = () => {
